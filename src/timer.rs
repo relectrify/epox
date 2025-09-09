59f6d8298be3e125dc976de489077bfde3d02f6e
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2025-09-09 10:38:35 +1000

    timer: use fd.with()

--- src/timer.rs
+++ src/timer.rs
@@ -1,7 +1,7 @@
 use crate::{EpollFlags, Fd, fd::AsFdWrapper};
 pub use nix::sys::timerfd::Expiration;
 use nix::sys::timerfd::{ClockId, TimerFd, TimerFlags, TimerSetTimeFlags};
-use std::{future::Future, io::Error, marker::PhantomData, os::fd::AsFd, pin::Pin, task::Poll};
+use std::{io::Error, marker::PhantomData, os::fd::AsFd};
 
 /*
  * Timer
@@ -25,31 +25,15 @@ impl Timer {
         Ok(())
     }
 
-    #[must_use]
-    pub const fn tick(&mut self) -> TimerFuture<'_> {
-        TimerFuture { timer: self }
-    }
-}
-
-/*
- * TimerFuture
- */
-pub struct TimerFuture<'a> {
-    timer: &'a mut Timer,
-}
-
-impl Future for TimerFuture<'_> {
-    type Output = Result<u64, Error>;
-
-    fn poll(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Self::Output> {
-        if self.timer.fd.poll_ready(cx).is_empty() {
-            return Poll::Pending;
-        }
-
-        let mut buf = std::mem::MaybeUninit::<[u8; 8]>::uninit();
-        /* safety: read does not require an initialised buffer */
-        nix::unistd::read(self.timer.fd.as_fd(), unsafe { buf.assume_init_mut() })?;
-        /* safety: if read() does not return an error, buf is initialised */
-        Poll::Ready(Ok(u64::from_ne_bytes(unsafe { *buf.assume_init_ref() })))
+    pub async fn tick(&mut self) -> Result<u64, Error> {
+        self.fd
+            .with(|fd, _events| {
+                let mut buf = std::mem::MaybeUninit::<[u8; 8]>::uninit();
+                /* safety: read does not require an initialised buffer */
+                nix::unistd::read(fd.as_fd(), unsafe { buf.assume_init_mut() })?;
+                /* safety: if read() does not return an error, buf is initialised */
+                Ok(u64::from_ne_bytes(unsafe { *buf.assume_init_ref() }))
+            })
+            .await
     }
 }
