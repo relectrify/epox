ea661eccd81e74ae6b71973632e0ca9d3f7093d7
Patrick Oppenlander <patrick.oppenlander@gmail.com>
2025-09-09 10:42:41 +1000

    signal: cleanup

--- src/signal.rs
+++ src/signal.rs
@@ -1,40 +1,74 @@
+use crate::Fd;
 pub use nix::sys::signal::{SigSet, Signal};
+use nix::sys::{
+    signal::{SigmaskHow, sigprocmask},
+    signalfd::{SfdFlags, SignalFd},
+};
+use std::{io::Error, os::fd::AsFd};
 
-use std::io::Error;
-
-pub struct SignalHandler {
-    fd: crate::fd::Fd<nix::sys::signalfd::SignalFd>,
+pub struct AsyncSignal {
+    fd: Fd<SignalFd>,
+    sigset: SigSet,
     _not_send_not_sync: core::marker::PhantomData<*mut ()>,
 }
 
-impl SignalHandler {
+/**
+ * Handle signals asynchronously.
+ *
+ * Receiving the same signal with multiple Signal instances is not
+ * supported.
+ *
+ * Attempting to receive a signal which is blocked will result in an error.
+ */
+impl AsyncSignal {
     pub fn new(sigset: SigSet) -> Result<Self, Error> {
-        // register sigset as signalfd
-        let signalfd = nix::sys::signalfd::SignalFd::with_flags(
-            &sigset,
-            nix::sys::signalfd::SfdFlags::SFD_NONBLOCK | nix::sys::signalfd::SfdFlags::SFD_CLOEXEC,
-        )?;
+        if sigset == SigSet::empty() {
+            return Err(Error::other("empty sigset"));
+        }
 
-        // ...then mask the signal so we can handle it
-        // todo: we should unmask on drop?
-        nix::sys::signal::sigprocmask(
-            nix::sys::signal::SigmaskHow::SIG_BLOCK,
-            Some(&sigset),
-            None,
-        )?;
+        /* make sure signals we're trying to receive are not blocked */
+        let mut oldset = SigSet::empty();
+        nix::sys::signal::sigprocmask(SigmaskHow::SIG_SETMASK, None, Some(&mut oldset))?;
+        for s in &sigset {
+            if oldset.contains(s) {
+                return Err(Error::other("signal is blocked"));
+            }
+        }
 
-        let fd = crate::fd::Fd::new(signalfd, nix::sys::epoll::EpollFlags::EPOLLIN)?;
+        let signalfd = SignalFd::with_flags(&sigset, SfdFlags::SFD_CLOEXEC)?;
+
+        /* signals received by signalfd need to be blocked to prevent them
+         * from being handled according to their default dispositions */
+        nix::sys::signal::sigprocmask(SigmaskHow::SIG_BLOCK, Some(&sigset), None)?;
 
         Ok(Self {
-            fd,
+            fd: Fd::new(signalfd, nix::sys::epoll::EpollFlags::EPOLLIN)?,
+            sigset,
             _not_send_not_sync: core::marker::PhantomData,
         })
     }
 
-    pub async fn received(&mut self) -> Result<Option<nix::libc::signalfd_siginfo>, Error> {
-        Ok(self
-            .fd
-            .with_fd(crate::EpollFlags::EPOLLIN, |fd| fd.read_signal())
-            .await?)
+    pub async fn receive(&mut self) -> Result<nix::libc::signalfd_siginfo, Error> {
+        self.fd
+            .with(|fd, _events| {
+                let mut buf = std::mem::MaybeUninit::<nix::libc::signalfd_siginfo>::uninit();
+                /* safety: read does not require an initialised buffer */
+                nix::unistd::read(fd.as_fd(), unsafe {
+                    std::slice::from_raw_parts_mut(
+                        buf.as_mut_ptr().cast::<u8>(),
+                        std::mem::size_of_val(&buf),
+                    )
+                })?;
+                /* safety: if read() does not return an error, buf is initialised */
+                Ok(unsafe { buf.assume_init() })
+            })
+            .await
+    }
+}
+
+impl Drop for AsyncSignal {
+    fn drop(&mut self) {
+        /* unblock signals as they will no longer be received by signalfd */
+        let _ = sigprocmask(SigmaskHow::SIG_UNBLOCK, Some(&self.sigset), None);
     }
 }
