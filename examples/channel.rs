914659c7e31486e29a4973227166af0d99756574
Alex Janka <alex@alexjanka.com>
2025-09-19 10:11:44 +1000

    async channel example and test

---
+++ examples/channel.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let (tx, rx) = async_channel::unbounded::<usize>();
+
+    let handle = epox::spawn_checked(async move {
+        let begin = std::time::Instant::now();
+        println!("waiting for message in async task...");
+        let received = rx.recv().await?;
+        let took = std::time::Instant::now().duration_since(begin);
+        println!("received '{received}' in async task; took {took:?}");
+        Ok::<_, Box<dyn std::error::Error>>(())
+    });
+
+    std::thread::spawn(move || {
+        println!("waiting 2s...");
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        println!("sending value from other thread");
+        tx.send_blocking(420).unwrap();
+    });
+
+    epox::run().unwrap();
+
+    handle
+        .result()
+        .expect("async task didn't complete")
+        .unwrap();
+}
